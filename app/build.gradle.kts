bac4c2c43dc8957b4a3d3cef7c517ace7c85bfba
fanny341 <fanny@example.com>
2026-07-07 13:45:16 +0800

    Fix build: use assembleDebug for simpler signing, remove package from manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,9 @@ android {
     }
 
     buildTypes {
+        debug {
+            isMinifyEnabled = false
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
